79208f179e659c351a11907b9c49477f9abf86f8
Max Trubetskoy <handcrab@gmail.com>
2015-02-13 20:28:05 +0300

    allow to edit and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all.order 'created_at DESC'
   end
 
   def show
-    @post = Post.find_by params[:id]
+    # @post = Post.find params[:id]
   end
 
 
@@ -22,10 +24,31 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    # @post = Post.find params[:id]
+  end
+
   def update
+    # @post = Post.find params[:id]
+    if @post.update post_params
+      redirect_to @post, notice: "Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    # @post = Post.find params[:id]
+    @post.destroy
+
+    redirect_to posts_path, notice: 'Success'
   end
 
   private
+    def set_post  
+      @post = Post.find params[:id]  
+    end
+
     def post_params
       params.require(:post).permit :title, :body
     end
