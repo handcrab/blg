5bdc641066cb52c32f1566c6165d80fef3ff919c
Max Trubetskoy <handcrab@gmail.com>
2015-02-13 18:06:49 +0300

    setup haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+
+  get 'posts/index'
+
+  get 'posts/show'
+
+  get 'posts/create'
+
+  get 'posts/update'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
